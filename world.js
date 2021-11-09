6e99898f9c94a1c22f539477a8b275fd8130281d
Solange Mohammed <solangemohammed123@gmail.com>
2021-11-09 11:42:26 -0400

    Completed exercise 1-3

---
+++ world.js
@@ -0,0 +1,40 @@
+window.onload = function()
+{
+    var httpRequest = new XMLHttpRequest();  
+    var url = "http://localhost/info2180-lab5/world.php?country="; 
+    var search = document.querySelector('#lookup'); 
+    var input = document.querySelector('input'); 
+    var result = document.querySelector('#result');  
+    search.addEventListener("click",function(element)
+    {  
+        element.preventDefault();
+        console.log("button clicked")   
+        httpRequest.onreadystatechange = function()
+        {
+            if(httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200){
+                var response =  httpRequest.responseText; 
+                console.log("provided response")
+                
+                if(input.value != ""){
+                    console.log(input)
+                    result.innerHTML = response
+                }else{
+                    console.log("Search")
+                    var response = httpRequest.responseText; 
+                    result.innerHTML = this.responseText; 
+                }
+
+            }
+
+                  
+        }
+    
+        httpRequest.open("GET", "http://localhost/info2180-lab5/world.php?country="+input.value); 
+        httpRequest.send();  
+    
+        
+        
+    });
+    
+
+}
