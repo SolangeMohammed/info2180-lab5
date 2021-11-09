93fa87a86afb968519364fae8cfb669e80834b0a
Solange Mohammed <solangemohammed123@gmail.com>
2021-11-09 13:25:45 -0400

    Added exercise 5

--- world.php
+++ world.php
@@ -11,14 +11,36 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+
+?>
+<?php
+if($country !== ""):
+echo "<table>
+
+<tr>
+  <th>Name</th>
+  <th>Continent</th>
+  <th> Independence </th>
+  <th> Head of State </th>
+</tr>"; ?>
+<?php foreach($results as $row):
+{
+echo "<tr>"; 
+echo "<td>" .$row['name']. "</td>";
+echo "<td>" .$row['continent']. "</td>";
+echo"<td>" .$row['independence_year']. "</td>";
+echo"<td>" .$row['head_of_state']. "</td>";
+echo "</tr>";
+  
+}
+endforeach;
+endif; 
+echo "</table>"; 
 ?>
 
 
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
 
 <?php
     header('Access-Control-Allow-Origin: *');
