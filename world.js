7509f6fcdc565d81c7d4f916c213953db4c71415
Solange Mohammed <solangemohammed123@gmail.com>
2021-11-10 00:22:50 -0400

    Added exercise 5

--- world.js
+++ world.js
@@ -3,6 +3,7 @@ window.onload = function()
     var httpRequest = new XMLHttpRequest();  
     var url = "http://localhost/info2180-lab5/world.php?country="; 
     var search = document.querySelector('#lookup'); 
+    var searchC = document.querySelector('#lookupCities')
     var input = document.querySelector('input'); 
     var result = document.querySelector('#result');  
     search.addEventListener("click",function(element)
@@ -31,9 +32,27 @@ window.onload = function()
     
         httpRequest.open("GET", "http://localhost/info2180-lab5/world.php?country="+input.value); 
         httpRequest.send();  
-    
         
-        
+    });
+
+    searchC.addEventListener("click", function(element)
+    {
+        element.preventDefault(); 
+        console.log("second button clicked")
+        httpRequest.onreadystatechange = function()
+        {
+            if(httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200){
+                var response =  httpRequest.responseText; 
+                console.log("provided response for city")
+                
+                result.innerHTML = response
+               
+            }          
+        }
+
+        httpRequest.open("GET", "http://localhost/info2180-lab5/world.php?context=cities&country="+input.value); 
+        httpRequest.send();  
+
     });
     
 
--- world.php
+++ world.php
@@ -3,43 +3,68 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
- 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country = $_GET["country"];
+$cities= $_GET["context"]; 
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
-
+$citysearch= $conn->query("SELECT countries.name AS countryname, cities.name AS city, cities.district, cities.population FROM countries, cities WHERE countries.code = cities.country_code"); 
+$results1 = $citysearch->fetchALL(PDO::FETCH_ASSOC); 
 ?>
+
 <?php
 if($country !== ""):
-echo "<table>
 
-<tr>
-  <th>Name</th>
-  <th>Continent</th>
-  <th> Independence </th>
-  <th> Head of State </th>
-</tr>"; ?>
-<?php foreach($results as $row):
-{
-echo "<tr>"; 
-echo "<td>" .$row['name']. "</td>";
-echo "<td>" .$row['continent']. "</td>";
-echo"<td>" .$row['independence_year']. "</td>";
-echo"<td>" .$row['head_of_state']. "</td>";
-echo "</tr>";
+  echo "<table>
+  <tr>
+    <th>Name</th>
+    <th>Continent</th>
+    <th> Independence </th>
+    <th> Head of State </th>
+  </tr>"; ?>
+  <?php foreach($results as $row):
+  {
+    echo "<tr>"; 
+    echo "<td>" .$row['name']. "</td>";
+    echo "<td>" .$row['continent']. "</td>";
+    echo"<td>" .$row['independence_year']. "</td>";
+    echo"<td>" .$row['head_of_state']. "</td>";
+    echo "</tr>";
   
-}
-endforeach;
+  }
+  endforeach;
 endif; 
 echo "</table>"; 
 ?>
 
+<?php
+if($cities == "cities"):
 
+  echo "<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th>
+    <th>Population</th>
+  </tr>"; ?>
+  <?php 
+  foreach($results1 as $row): {
+    if($row['countryname']== $country || $country == ""):
+      {
+        echo "<tr>"; 
+        echo "<td>" .$row['city']. "</td>";
+        echo "<td>" .$row['district']. "</td>";
+        echo"<td>" .$row['population']. "</td>";
+        echo "</tr>";  
+
+      }
+    endif; 
+  }
+  endforeach;
+endif; 
+echo "</table>"; 
+?>
 
 
 <?php
